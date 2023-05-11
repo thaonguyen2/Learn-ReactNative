e66d7f3a01c38a85e4c81ad88f7b3882244ac4cb
Thao Nguyen <thaonh@tohsoft.com>
2023-05-11 15:45:41 +0700

    Update

--- TodoApp/TabBar.js
+++ TodoApp/TabBar.js
@@ -1,28 +1,31 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import TabBarItem from "./TabBarItem";
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import TabBarItem from './TabBarItem';
 
-const TabBar = ({ setType, type }) => (
-    <View style={styles.container}>
-        <TabBarItem type={type} title='All'
-        setType={() => setType('All')}>
-        </TabBarItem>
-        <TabBarItem type={type} title='Active'
-        setType={() => setType('Active')}>
-        </TabBarItem>
-        <TabBarItem type={type} title='Complete'
-        setType={() => setType('Complete')}>
-        </TabBarItem>
-    </View>
-)
+const TabBar = ({setType, type}) => (
+  <View style={styles.container}>
+    <TabBarItem
+      type={type}
+      title="All"
+      setType={() => setType('All')}></TabBarItem>
+    <TabBarItem
+      type={type}
+      title="Active"
+      setType={() => setType('Active')}></TabBarItem>
+    <TabBarItem
+      type={type}
+      title="Complete"
+      setType={() => setType('Complete')}></TabBarItem>
+  </View>
+);
 
 const styles = StyleSheet.create({
-    container: {
-        height: 70,
-        flexDirection: 'row',
-        borderTopWidth: 1,
-        borderTopColor: '#dddddd'
-    }
-})
+  container: {
+    height: 70,
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderTopColor: '#dddddd',
+  },
+});
 
-export default TabBar
+export default TabBar;
